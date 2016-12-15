19b8f149bdbee47768850762288ab00b6c87a262
Derek Palmer <dp462@cornell.edu>
2016-12-14 21:14:32 -0500

    Cloned files from scotch example repo for troubleshooting

--- app/database.js
+++ app/database.js
@@ -3,17 +3,18 @@ var loki = require('lokijs');
 var db = new loki('db.json');
 
 db.addCollection('top').insert([
-    {term: '370z',      style:'warning'},
-    {term: 'Z',         style:'danger'},
-    {term: 'Fairlady',  style:'success'},
-    {term: '350z',      style:'info'},
-    {term: '300zx',     style:'default'},
-    {term: '240z',      style:'warning'},
-    {term: '280zx',     style:'info'},
-    {term: 'Z34',       style:'primary'},
-    {term: 'nismo',     style:'default'},
-    {term: 'nissan',    style:'success'},
-  ]);
+  {term:'JavaScript'      , style :'warning' },
+  {term:'Angular 2'       , style :'danger'  },
+  {term:'NodeJS'          , style :'success' },
+  {term:'Golang'          , style :'info'    },
+  {term:'iOS'             , style :'default' },
+  {term:'ReactJS'         , style :'warning' },
+  {term:'Ionic'           , style :'info'    },
+  {term:'REST'            , style :'primary' },
+  {term:'Authentication'  , style :'default' },
+  {term:'Android'         , style :'success'}
+]);
 
-  db.addCollection('searches');
-  db.saveDatabase();
+db.addCollection('searches');
+
+db.saveDatabase();
